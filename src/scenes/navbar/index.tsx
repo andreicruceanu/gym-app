a9fb3894897ef51b0db393d3d964f06644306d07
Cruceanu Andrei <cruceanuandrei10@gmail.com>
2024-08-07 22:08:21 +0300

    Navbar finish

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,39 +3,90 @@ import Logo from "@/assets/Logo.png";
 import links from "@/utils/links";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { useState } from "react";
+import ActionButton from "@/shared/ActionButton";
 
 type NavbarProps = {
+  isTopOfPage: boolean;
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: NavbarProps) => {
+const Navbar = ({
+  isTopOfPage,
+  selectedPage,
+  setSelectedPage,
+}: NavbarProps) => {
   const flexBetween = "flex items-center justify-between";
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-14`}>
             <img alt="logo" src={Logo} />
-            <div className={`${flexBetween} w-full`}>
-              <div className={`${flexBetween} gap-8 text-sm`}>
-                {links.map((item) => (
-                  <Link
-                    page={item.label}
-                    selectedPage={selectedPage}
-                    setSelectedPage={setSelectedPage}
-                  />
-                ))}
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  {links.map((item) => (
+                    <Link
+                      key={item.label}
+                      page={item.label}
+                      selectedPage={selectedPage}
+                      setSelectedPage={setSelectedPage}
+                    />
+                  ))}
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
               </div>
-              <div className={`${flexBetween} gap-8`}>
-                <p>Sign In</p>
-                <button>Become a Member</button>
-              </div>
-            </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
         </div>
       </div>
+
+      {/* MOBILE MENU MODAL  */}
+
+      {/* MOBILE MENU MODAL */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl ">
+          {/* CLOSE ICON */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+
+          {/* MENU ITEMS */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            {links.map((item) => (
+              <Link
+                key={item.label}
+                page={item.label}
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
